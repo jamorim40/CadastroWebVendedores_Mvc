c0946159939d6fb738b71337763a3cddc7896f45
Juliano Amorim <jamorim40@outlook.com>
2026-01-12 13:39:56 -0300

    Classe Controller Vendedor

---
+++ CadastroWebVendedores_Mvc/Controllers/VendedoresController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroWebVendedores_Mvc.Controllers
+{
+    public class VendedoresController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
